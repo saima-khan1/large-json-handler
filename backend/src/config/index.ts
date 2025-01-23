5274a08799d0003fe32acfff38bd0891c3225a77
Saima Sanjeeda Khan <119809514+saima-khan1@users.noreply.github.com>
2025-01-23 18:51:09 +0100

    updated port number in config file

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -3,6 +3,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const config = {
-  port: process.env.PORT || 5000,
+  port: process.env.PORT || 3000,
   apiUrl: process.env.API_URL || "",
 };
