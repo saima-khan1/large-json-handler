d7ebaf1e4543d1abc62dc642e40c2c68a6405a4c
Saima Sanjeeda Khan <119809514+saima-khan1@users.noreply.github.com>
2025-02-12 10:42:59 +0100

    fixed cors issue

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,16 +2,34 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import { config } from "./config";
 import { baseRoutes } from "./routes";
+import bodyParser from "body-parser";
 
 const app = express();
 
-const corsConfig = cors({
-  origin: config.FRONTEND_URL,
-  methods: "GET, POST, OPTIONS",
-  allowedHeaders: "*",
-});
+// const corsConfig = cors({
+//   origin: config.FRONTEND_URL,
+//   methods: "GET, POST, OPTIONS",
+//   allowedHeaders: "*",
+// });
 
-app.use(corsConfig);
+const originWhitelist = [
+  "http://localhost:5173",
+  "https://large-json-handler-client-dun.vercel.app",
+  "null",
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (originWhitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.json());
 
